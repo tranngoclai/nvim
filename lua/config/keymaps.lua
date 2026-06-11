1f9af5e63da0de0b02137b23a3b42ed55769f885
lai.tran <lai.tran@executionlab.asia>
2026-06-11 17:28:23 +0700

    fix(keymaps): stabilize leader key hints

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,6 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "W", function()
-  Snacks.bufdelete()
-end, { desc = "Close Buffer" })
+vim.keymap.set("n", "W", "<leader>bd", { remap = true, desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>bn", "<cmd>enew<cr>", { desc = "New Buffer" })
+vim.keymap.set("n", "N", "<cmd>enew<cr>", { desc = "New Buffer" })
---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "folke/which-key.nvim",
+    -- Load before VeryLazy so the <leader> trigger exists as soon as editing starts.
+    event = "VimEnter",
+  },
+}
